ccd2344d21b6fb5e34283738daec3de4ad33b334
Mariana Marcelino <mariana_c_marcelino@hotmail.com>
2018-09-27 16:56:15 +0100

    deleted extra favourites file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :favourites
+  has_many :videos, through: :favourites
+  validates :mail, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create
+  validates :username, presence: true, on: :create
 end
--- db/migrate/20180927151956_create_favourites.rb
+++ db/migrate/20180927151956_create_favourites.rb
@@ -1,8 +1,8 @@
 class CreateFavourites < ActiveRecord::Migration[5.1]
   def change
     create_table :favourites do |t|
-      t.references :user_id, foreign_key: true
-      t.references :video_id, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :video, foreign_key: true
 
       t.timestamps
     end
