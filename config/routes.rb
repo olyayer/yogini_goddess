329ddf210176408fb8b88898b9d23a3de4053923
Mariana Marcelino <mariana_c_marcelino@hotmail.com>
2018-10-02 16:14:27 +0100

    redid evrything again

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get 'videos', to: 'videos#index'
+  get 'videos/:id', to: 'videos#show', as: :video
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
