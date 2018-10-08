57141032b8c5d87cf18c2e83616637ba081d78c3
Olya Yeremenko <oyeremenko@gmail.com>
2018-10-08 15:58:56 +0100

    scraping in process

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,9 +2,34 @@ class VideosController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+
+    require 'open-uri'
+    require 'nokogiri'
+
+    search_keyword = 'yoga'
+
+    url = "https://www.youtube.com/results?search_query=#{search_keyword}"
+    doc = open(url).read
+    parsed_doc = Nokogiri::HTML(doc)
+
+    video_infos = parsed_doc.search('.ytd-simple-endpoint .style-scope .ytd-video-renderer').take(10)
+    videosArray = []
+
+
+    video_infos.each do |video|
+      link = video.attribute('href').value
+      # @videosArray << Video.new(link)
+
+      puts link
+    end
+
+
+
+
   end
 
   def show
+
   end
 
 
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,11 @@
 class Video < ApplicationRecord
 
+  attr_reader :link
+
+  def initialize(link)
+    @link = link
+  end
+
+
+
 end
