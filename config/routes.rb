9238f302f5060c8743eb1058c5ce70bdd8397f9e
Mariana Marcelino <mariana_c_marcelino@hotmail.com>
2018-10-09 16:15:50 +0100

    struggling with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'videos#index'
 
-    # resources :videos, :only [:index, :show]
-    get 'videos', to: 'videos#index'
-    get 'videos/:id', to: 'videos#show', as: :video
+    resources :videos, :only [:index, :new, :create]
+    # get 'videos', to: 'videos#index'
+    # get 'videos/:id', to: 'videos#show', as: :video
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
